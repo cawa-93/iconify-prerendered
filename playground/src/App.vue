124153482120a515955ed849f6efc98aa51a9e0f
Alex Kozack <cawa-93@users.noreply.github.com>
2022-08-16 23:49:43 +0300

    chore: cleanup

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
+import {IconUniversalAccess} from '@iconify-prerendered/vue-bi'
 </script>
 
 <template>
   <div>
-    <IconZoomInRounded class="icon" style="width: 20px"/>
+    <IconUniversalAccess class="icon" style="width: 20px"/>
   </div>
 </template>
 
