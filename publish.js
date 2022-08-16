548d8912faa7e2e13afa14d1a2222edcb9c7b190
Alex Kozack <cawa-93@users.noreply.github.com>
2022-08-16 23:36:35 +0300

    chore: publish readme

--- publish.js
+++ publish.js
@@ -1,10 +1,11 @@
 import npmPublish from "@jsdevtools/npm-publish";
 import fs from 'node:fs'
 
-const packages = fs.readdirSync('./dist').map(d => `./dist/${d}/package.json`)
+const packages = fs.readdirSync('./dist').map(d => `./dist/${d}`)
 for (const pack of packages) {
+    await fs.copyFile('./README.md', `${pack}/README.md`, fs.constants.COPYFILE_FICLONE)
     await npmPublish({
-        package: pack,
+        package: `${pack}/package.json`,
         access: 'public',
         checkVersion: true,
         token: process.env.NPM_TOKEN,
