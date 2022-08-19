b9aaffdde31feaf615805b0dffe29530ac81e397
Alex Kozack <cawa-93@users.noreply.github.com>
2022-08-19 19:48:56 +0300

    ci(publish): remove readme coping

--- publish.js
+++ publish.js
@@ -3,7 +3,6 @@ import fs from 'node:fs'
 
 const packages = fs.readdirSync('./dist').map(d => `./dist/${d}`)
 for (const pack of packages) {
-  await fs.promises.copyFile('./README.md', `${pack}/README.md`, fs.constants.COPYFILE_FICLONE)
   await npmPublish({
     package: `${pack}/package.json`,
     access: 'public',
