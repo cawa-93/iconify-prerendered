8212f03f192d8d367c777014555d58ab59f7b01d
Alex Kozack <cawa-93@users.noreply.github.com>
2022-09-03 13:19:54 +0300

    test: Add more tests

--- tests/build.spec.js
+++ tests/build.spec.js
@@ -3,22 +3,59 @@ import {getIconData, iconToSVG} from "@iconify/utils";
 import {test} from "@japa/runner";
 import esmock from "esmock";
 import {getComponentName} from "../getComponentName.js";
+import * as fs from "node:fs";
+import * as path from "node:path";
 
 const collections = Object.keys(await lookupCollections())
 
 
+function getCollectionFile(prefix, file) {
+  return path.resolve('dist', prefix, file);
+}
+
 function importIconSet(prefix) {
-  return esmock(`../dist/${prefix}/index.js`, {
+  return esmock(getCollectionFile(prefix, 'index.js'), {
     vue: {h: (...args) => args}
   }, {}, {
     isPackageNotFoundError: false
   });
 }
 
+function isFileExist(filepath) {
+  return fs.promises.access(filepath, fs.constants.F_OK)
+    .then(() => true)
+    .catch(() => false)
+}
+
+function isFileEmpty(filepath) {
+  return fs.promises.readFile(filepath, {encoding: 'utf8'})
+    .then((content) => content.trim().length === 0)
+}
+
+function isValidJsonFile(filepath) {
+  return fs.promises.readFile(filepath, {encoding: 'utf8'})
+    .then(JSON.parse)
+    .then(() => true)
+    .catch(() => false)
+}
+
 for (const collectionPrefix of collections) {
   const set = await importIconSet(collectionPrefix)
 
   test.group(`validate collection ${collectionPrefix}`, () => {
+    test('should have {$self}')
+      .with(['README.md', 'package.json'])
+      .run(async ({assert}, file) => {
+        const filePath = getCollectionFile(collectionPrefix, file)
+
+        assert.equal(await isFileExist(filePath), true, 'File exist check')
+        assert.equal(await isFileEmpty(filePath), false, 'File not empty check')
+
+        if (filePath.endsWith('json')) {
+          assert.equal(await isValidJsonFile(filePath), true, 'JSON validation')
+        }
+      })
+
     test(`validate ${collectionPrefix}/{name}`)
       .with(async () => {
         const collection = await lookupCollection(collectionPrefix)
