e50dd0b7de42951a37e1dd521de487cf0b20f7c2
Олександр Козак <kozackunisoft@gmail.com>
2023-07-26 12:05:32 +0300

    ci: try to configure npm provenance

--- tasks/publish.ts
+++ tasks/publish.ts
@@ -1,18 +1,18 @@
-import npmPublish from 'npm:@jsdevtools/npm-publish';
+import { npmPublish } from 'npm:@jsdevtools/npm-publish@2.2.1';
 import { join } from 'https://deno.land/std@0.176.0/path/mod.ts';
 
 const entry = join(Deno.cwd(), '/generated/');
 
-const token = Deno.env.get('NPM_TOKEN');
+const token = Deno.env.get('NODE_AUTH_TOKEN');
 
 if (!token) {
   if (Deno.env.get('CI') && Deno.env.get('CI') !== 'false') {
     throw new Error(
-      `Expected NPM_TOKEN as string but got ${JSON.stringify(token)}`,
+      `Expected NODE_AUTH_TOKEN as string but got ${JSON.stringify(token)}`,
     );
   } else {
     console.error(
-      `NPM_TOKEN is ${JSON.stringify(token)}. Forced to dry-run publishing`,
+      `NODE_AUTH_TOKEN is ${JSON.stringify(token)}. Forced to dry-run publishing`,
     );
   }
 }
@@ -25,8 +25,8 @@ for (const dirEntry of Deno.readDirSync(entry)) {
   await npmPublish({
     package: join(entry, dirEntry.name, `package.json`),
     access: 'public',
-    checkVersion: true,
-    token,
+    token: token || 'dry-run-token-placeholder',
     dryRun: !token,
-  });
+    provenance: true
+});
 }
